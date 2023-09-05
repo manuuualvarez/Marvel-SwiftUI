5b26c33d36619340ffe0ae5b5daaf474781d176e
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2023-09-05 20:43:32 -0300

    Initial Commit

---
+++ Marvel/MarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelApp.swift
+//  Marvel
+//
+//  Created by Manny Alvarez on 05/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
